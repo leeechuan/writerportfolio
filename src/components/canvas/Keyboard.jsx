e72270736fe7d52bc36fccf8050ed8bbaa6b9910
leeechuan <aleechuan@gmail.com>
2024-03-03 17:08:02 +0800

    optimsation for spline 03032023

--- src/components/canvas/Keyboard.jsx
+++ src/components/canvas/Keyboard.jsx
@@ -2,68 +2,71 @@ import React, { useState, useEffect, Suspense } from 'react';
 import { Application } from '@splinetool/runtime';
 import gsap from "gsap";
 import CanvasLoader from "../Loader";
+import keyboard from "../../assets/keyboard.png"
 
 const KeyboardAnimation = () => {
-
-
-
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    
     const canvas = document.getElementById("canvas3d");
-    const app = new Application(canvas);
 
-    app
-      .load("https://prod.spline.design/uZEocBpkqwKrhJ0D/scene.splinecode")
-      .then(() => {
-        app.camera.controls.enableZoom = false;
-        setIsLoading(false); // Once the model is loaded, set isLoading to false
-        startKeyPressAfterRender(); // Start keyboard animation
-      })
-
-
-
-      const targetString = " Lee Chuan";
-      let currentIndex = 0;
-      let keyPressInterval;
-
-      
-      // Function to simulate keydown event
-      function simulateKeyDown(char) {
-        const event = new KeyboardEvent("keydown", {
-          key: char,
-          bubbles: true
+    if (isMacOrAndroid()) {
+      // If the user's OS is Mac or Android, show an image instead of using spline code
+      const image = document.createElement("img");
+      image.src = keyboard; // Replace "path_to_your_image.jpg" with the actual path to your image
+      image.onload = () => {
+        canvas.parentNode.replaceChild(image, canvas);
+        setIsLoading(false);
+      };
+    } else {
+      const app = new Application(canvas);
+      app
+        .load("https://prod.spline.design/uZEocBpkqwKrhJ0D/scene.splinecode")
+        .then(() => {
+          app.camera.controls.enableZoom = false;
+          setIsLoading(false);
+          startKeyPressAfterRender();
         });
-        console.log(char)
-        document.dispatchEvent(event);
-      }
-      
-      // Function to start simulating key presses after render of the spline model
-      function startKeyPressAfterRender() {
-        currentIndex = 0; // Reset currentIndex
-        keyPressInterval = setInterval(() => {
-          simulateKeyDown(targetString[currentIndex]);
-          currentIndex++;
-          if (currentIndex === targetString.length) {
-            clearInterval(keyPressInterval);
-            console.log(currentIndex)
-      
-          }
-        }, 700); // Each keypress interval
-      }
-      
-      // Start simulating key presses after render of the spline model
-      setTimeout(startKeyPressAfterRender,3000);  
+    }
 
+    function isMacOrAndroid() {
+      const platform = navigator.platform.toLowerCase();
+      return platform.includes("mac") || platform.includes("android");
+    }
+
+    const targetString = " Lee Chuan";
+    let currentIndex = 0;
+    let keyPressInterval;
+
+    function simulateKeyDown(char) {
+      const event = new KeyboardEvent("keydown", {
+        key: char,
+        bubbles: true
+      });
+      console.log(char);
+      document.dispatchEvent(event);
+    }
+
+    function startKeyPressAfterRender() {
+      currentIndex = 0;
+      keyPressInterval = setInterval(() => {
+        simulateKeyDown(targetString[currentIndex]);
+        currentIndex++;
+        if (currentIndex === targetString.length) {
+          clearInterval(keyPressInterval);
+          console.log(currentIndex);
+        }
+      }, 500);
+    }
+
+    setTimeout(startKeyPressAfterRender, 1000);
   }, []);
 
   return (
     <div className="canvas-cont">
-    <canvas id="canvas3d"></canvas>
+      <canvas id="canvas3d"></canvas>
     </div>
-  ); // Since this component doesn't render anything directly
+  );
 };
 
 export default KeyboardAnimation;
-
